8ec0c81718078f77f6290541798a4933f5281895
fengxiaofu <xiaofufeng2003@163.com>
2018-11-03 01:41:05 +0800

    update

--- ass5/scriptlib2.js
+++ ass5/scriptlib2.js
@@ -72,24 +72,26 @@ var table2_items = [];
                           defaultContent:""},
                     ] // rmf columns
                 } ); // end dataTable
+            
+
+
+
+    var chart = c3.generate({
+                         data: {
+                             columns: table2_dataSet,
+                             type : 'bar'
+                         },
+                         bar: {
+                             title: "Tasks for Each Stage:",
+                         }
+                     });
+
               }); // end .getJSON
-
-    }); // end button
-
-$("button#see_chart").click(function() {
-                var chart = c3.generate({
-                     data: {
-                         columns: table2_dataSet,
-                         type : 'bar'
-                     },
-                     bar: {
-                         title: "Outfits for Each Occasions:",
-                     }
-                 });
+           }); // end button
 
 
 
-}); // end button
+
 
 
 

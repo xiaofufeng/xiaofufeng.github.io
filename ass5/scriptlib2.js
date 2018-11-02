ee714079c5df116051d693d2ad6627c7ed674fee
fengxiaofu <xiaofufeng2003@163.com>
2018-11-03 00:18:46 +0800

    update

--- ass5/scriptlib2.js
+++ ass5/scriptlib2.js
@@ -1,44 +1,50 @@
 $(document).ready(function(){
 
-    $("button#roll_up").click(function() {
-       var table1_items = [];
-       var i = 0;
-       var airtable_read_endpoint = "https://api.airtable.com/v0/appENaVzPyNLQlumN/Outfits?api_key=keytLf7m73sOFf472&maxRecords=35&view=Grid%20view";
-       var table1_dataSet = [];
-       $.getJSON(airtable_read_endpoint, function(result) {
-              $.each(result.records, function(key,value) {
-                  table1_items = [];
-                      table1_items.push(value.fields.Outfits_code);
-                      table1_items.push(value.fields.Occasions_);
-                      table1_items.push(value.fields.Total_price);
-                      table1_items.push(value.fields.Money_saved);
-                      table1_items.push(value.fields.Tops_price);
-                      table1_items.push(value.fields.Bottoms_price);
-                      table1_dataSet.push(table1_items);
-                      console.log(table1_items);
-               }); // end .each
-               console.log(table1_dataSet);
+  $("button#get_data").click(function() {
+     var table1_items = [];
+     var i = 0;
+     var airtable_read_endpoint = "https://api.airtable.com/v0/appENaVzPyNLQlumN/Outfits?api_key=keytLf7m73sOFf472&maxRecords=35&view=Grid%20view";
+     var table1_dataSet = [];
+     $.getJSON(airtable_read_endpoint, function(result) {
+            $.each(result.records, function(key,value) {
+                table1_items = [];
+                    table1_items.push(value.fields.Outfits_code);
+                    table1_items.push(value.fields.Occasions_);
+                    table1_items.push(value.fields.Total_price);
+                    table1_items.push(value.fields.Money_saved);
+                    table1_items.push(value.fields.Tops_price);
+                    table1_items.push(value.fields.Bottoms_price);
+                    table1_dataSet.push(table1_items);
+                    console.log(table1_items);
+             }); // end .each
+             console.log(table1_dataSet);
 
-            $('#table1').DataTable( {
-                data: table1_dataSet,
-                retrieve: true,
-                columns: [
-                    { title: "Outfits_code",
-                      defaultContent:""},
-                    { title: "Occasions_",
-                        defaultContent:"" },
-                    { title: "Total_price",
+          $('#table1').DataTable( {
+              data: table1_dataSet,
+              retrieve: true,
+              columns: [
+                  { title: "Outfits_code",
+                    defaultContent:""},
+                  { title: "Occasions_",
                       defaultContent:"" },
-                    { title: "Money_saved",
+                  { title: "Total_price",
+                    defaultContent:"" },
+                  { title: "Money_saved",
+                    defaultContent:""},
+                  { title: "Tops_price",
                       defaultContent:""},
-                    { title: "Tops_price",
-                        defaultContent:""},
-                    { title: "Bottoms_price",
-                      defaultContent:""},
-                ]
-            } );
-       }); // end .getJSON
-//roll up table2 and chart
+                  { title: "Bottoms_price",
+                    defaultContent:""},
+              ]
+          } );
+     }); // end .getJSON
+}); // end button
+
+
+
+    $("button#roll_up").click(function() {
+
+//roll up table2
 var table2_items = [];
          var i = 0;
          var airtable_read_endpoint =
@@ -64,7 +70,11 @@ var table2_items = [];
                           defaultContent:""},
                     ] // rmf columns
                 } ); // end dataTable
+              }); // end .getJSON
 
+    }); // end button
+
+$("button#roll_up").click(function() {
                 var chart = c3.generate({
                      data: {
                          columns: table2_dataSet,
@@ -75,7 +85,7 @@ var table2_items = [];
                      }
                  });
 
-          }); // end .getJSON
+
 
 }); // end button
 

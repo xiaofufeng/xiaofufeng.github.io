62df226d06fe25fec31a7b5040e14b75092489de
fengxiaofu <xiaofufeng2003@163.com>
2018-11-02 20:26:35 +0800

    update

--- ass5/scriptlib2.js
+++ ass5/scriptlib2.js
@@ -1,27 +1,27 @@
 $(document).ready(function(){
 
     $("button#get_data").click(function() {
-        var items = [];
+        var table1_items = [];
         var i = 0;
 
         var airtable_read_endpoint = "https://api.airtable.com/v0/appENaVzPyNLQlumN/Outfits?api_key=keytLf7m73sOFf472&maxRecords=35&view=Grid%20view";
-        var dataSet = [];
+        var table1_dataSet = [];
         $.getJSON(airtable_read_endpoint, function(result) {
                $.each(result.records, function(key,value) {
                    items = [];
-                       items.push(value.fields.Outfits_code);
-                       items.push(value.fields.Occasions_);
-                       items.push(value.fields.Total_price);
-                       items.push(value.fields.Money_saved);
-                       items.push(value.fields.Tops_price);
-                       items.push(value.fields.Bottoms_price);
+                       table1_items.push(value.fields.Outfits_code);
+                       table1_items.push(value.fields.Occasions_);
+                       table1_items.push(value.fields.Total_price);
+                       table1_items.push(value.fields.Money_saved);
+                       table1_items.push(value.fields.Tops_price);
+                       table1_items.push(value.fields.Bottoms_price);
                        dataSet.push(items);
                        console.log(items);
                 }); // end .each
                 console.log(dataSet);
 
-             $('#example').DataTable( {
-                 data: dataSet,
+             $('#table1').DataTable( {
+                 data: table1_dataSet,
                  retrieve: true,
                  columns: [
                      { title: "Outfits_code",
@@ -42,7 +42,6 @@ $(document).ready(function(){
      }); // end button
 
 //roll up table 1
-
 
 
 

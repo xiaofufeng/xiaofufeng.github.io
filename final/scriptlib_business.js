adb3a55ef38bc30ec70fbf6f22f84b4eccc38589
fengxiaofu <xiaofufeng2003@163.com>
2018-11-13 20:48:43 +0800

    update

--- final/scriptlib_business.js
+++ final/scriptlib_business.js
@@ -98,7 +98,7 @@ var table2_items = [];
                               title: "Total Price for Each Occasion:",
 
                           width: {
-                                      ratio:0.5 // this makes bar width 50% of length between ticks
+                                      ratio:0.3 // this makes bar width 50% of length between ticks
                                   },
                                   // or
                                   //width: 100 // this makes bar width 100px

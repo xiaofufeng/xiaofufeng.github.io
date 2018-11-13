8c5ea64b4ba584df41d2c7985a6c0ae4abb03be2
fengxiaofu <xiaofufeng2003@163.com>
2018-11-13 21:52:21 +0800

    update

--- final/scriptlib_business.js
+++ final/scriptlib_business.js
@@ -106,65 +106,62 @@ $("#div1").hide(1000);
                     }); // end button
 
         //see chart of total price
-        $("button#see_total_price").click(function() {
-        $("#div1").hide(1000);
-        
 
-        var table2_items = [];
-                 var i = 0;
-                 var airtable_read_endpoint =
-                 "https://api.airtable.com/v0/appENaVzPyNLQlumN/Business%26Interview?api_key=keytLf7m73sOFf472&maxRecords=40&view=Grid%20view";
-                 var table2_dataSet = [];
-                 $.getJSON(airtable_read_endpoint, function(result) {
-                        $.each(result.records, function(key,value) {
-                            table2_items = [];
-                                table2_items.push(value.fields.Outfits_code);
-                                table2_items.push(value.fields.Total_price);
-                                table2_dataSet.push(table2_items);
-                                console.log(table2_items);
-                         }); // end .each
-                         console.log(table2_dataSet);
-                        $('#table2').DataTable( {
-                            data: table2_dataSet,
-                            retrieve: true,
-                            ordering: false,
-                            columns: [
-                                { title: "Outfits_code",
-                                  defaultContent:""},
-                                { title: "Total_price",
-                                  defaultContent:""},
-                            ] // rmf columns
-                        } ); // end dataTable
+              $("button#see_total_price").click(function() {
+     $("#div1").hide(1000);
+
+              var table2_items = [];
+                       var i = 0;
+                       var airtable_read_endpoint =
+                       "https://api.airtable.com/v0/appENaVzPyNLQlumN/Business%26Interview?api_key=keytLf7m73sOFf472&maxRecords=40&view=Grid%20view";
+                       var table2_dataSet = [];
+                       $.getJSON(airtable_read_endpoint, function(result) {
+                              $.each(result.records, function(key,value) {
+                                  table2_items = [];
+                                      table2_items.push(value.fields.Outfits_code);
+                                      table2_items.push(value.fields.Total_price);
+                                      table2_dataSet.push(table2_items);
+                                      console.log(table2_items);
+                               }); // end .each
+                               console.log(table2_dataSet);
+                              $('#table2').DataTable( {
+                                  data: table2_dataSet,
+                                  retrieve: true,
+                                  ordering: false,
+                                  columns: [
+                                      { title: "Outfits_code",
+                                        defaultContent:""},
+                                      { title: "Total_price",
+                                        defaultContent:""},
+                                  ] // rmf columns
+                              } ); // end dataTable
 
 
-            var chart = c3.generate({
-                                 data: {
-                                     columns: table2_dataSet,
-                                     type : 'bar'
-                                 },
-                                 color: {
-                                         pattern: ['#1f77b4']
-                                     },
-                                 axis: {
-                                             x: {label: 'Outfits code'
-
+                  var chart = c3.generate({
+                                       data: {
+                                           columns: table2_dataSet,
+                                           type : 'bar'
+                                       },
+                                       color: {
+                                               pattern: ['#1f77b4']
                                            },
-                                             y: {label: '# of Total price'}
-                                         },
-                                  bar: {
-                                      title: "Total Price for Each Occasion:",
+                                       axis: {
+                                                   x: {label: 'Outfits code'},
+                                                   y: {label: '# of Total Price'}
+                                                 },
+                                              bar: {
+                                                  title: "Total Price for Each Occasion:",
 
-                                  width: {
-                                              ratio:0.4 // this makes bar width 50% of length between ticks
-                                          },
-                                          // or
-                                          //width: 100 // this makes bar width 100px
-                                        },
-                             });
+                                              width: {
+                                                          ratio:0.4 // this makes bar width 50% of length between ticks
+                                                      },
+                                                      // or
+                                                      //width: 100 // this makes bar width 100px
+                                                     },
+                                   });
 
-                      }); // end .getJSON
-                   }); // end button
-
+                            }); // end .getJSON
+                         }); // end button
 
 
 }); // document ready

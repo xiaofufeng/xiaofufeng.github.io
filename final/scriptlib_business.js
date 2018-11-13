00825ce3f7e8d80ede04e96efd558b32d80218b4
fengxiaofu <xiaofufeng2003@163.com>
2018-11-13 20:53:39 +0800

    update

--- final/scriptlib_business.js
+++ final/scriptlib_business.js
@@ -98,7 +98,7 @@ var table2_items = [];
                               title: "Total Price for Each Occasion:",
 
                           width: {
-                                      ratio:0.3 // this makes bar width 50% of length between ticks
+                                      ratio:0.4 // this makes bar width 50% of length between ticks
                                   },
                                   // or
                                   //width: 100 // this makes bar width 100px
@@ -156,7 +156,7 @@ $("#chart").hide(1000);
                                              title: "Money Saved for Each Occasion:",
 
                                          width: {
-                                                     ratio:0.1 // this makes bar width 50% of length between ticks
+                                                     ratio:0.4 // this makes bar width 50% of length between ticks
                                                  },
                                                  // or
                                                  //width: 100 // this makes bar width 100px

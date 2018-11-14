14cac735cf696613ee93fba19d57da35cd570772
fengxiaofu <xiaofufeng2003@163.com>
2018-11-15 02:06:16 +0800

    update

--- final/scriptlib_shirts.js
+++ final/scriptlib_shirts.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 
-  $("button#t_shirts").click(function() {
+  $("button#shirts").click(function() {
     $("#div1").show(1000);
     $("#footer").show(1000);
     $("#how_to_match").show(1000);
     $("#see_chart").show(1000);
      var table1_items = [];
      var i = 0;
-     var airtable_read_endpoint = "https://api.airtable.com/v0/appENaVzPyNLQlumN/final_t_shirts?api_key=keytLf7m73sOFf472&maxRecords=40&view=Grid%20view";
+     var airtable_read_endpoint = "https://api.airtable.com/v0/appENaVzPyNLQlumN/final_shirts?api_key=keytLf7m73sOFf472&maxRecords=40&view=Grid%20view";
      var table1_dataSet = [];
      $.getJSON(airtable_read_endpoint, function(result) {
             $.each(result.records, function(key,value) {
@@ -86,7 +86,7 @@ $("button#how_to_match").click(function() {
   $("#see_chart").show(1000);
    var table2_items = [];
    var i = 0;
-   var airtable_read_endpoint = "https://api.airtable.com/v0/appENaVzPyNLQlumN/final_t_shirts?api_key=keytLf7m73sOFf472&maxRecords=40&view=Grid%20view";
+   var airtable_read_endpoint = "https://api.airtable.com/v0/appENaVzPyNLQlumN/final_shirts?api_key=keytLf7m73sOFf472&maxRecords=40&view=Grid%20view";
    var table2_dataSet = [];
    $.getJSON(airtable_read_endpoint, function(result) {
           $.each(result.records, function(key,value) {
@@ -164,7 +164,7 @@ $("#footer").show(1000);
          var table3_items = [];
                   var i = 0;
                   var airtable_read_endpoint =
-                  "https://api.airtable.com/v0/appENaVzPyNLQlumN/final_t_shirts?api_key=keytLf7m73sOFf472&maxRecords=40&view=Grid%20view";
+                  "https://api.airtable.com/v0/appENaVzPyNLQlumN/final_shirts?api_key=keytLf7m73sOFf472&maxRecords=40&view=Grid%20view";
                   var table3_dataSet = [];
                   $.getJSON(airtable_read_endpoint, function(result) {
                          $.each(result.records, function(key,value) {
@@ -226,7 +226,72 @@ $("#footer").show(1000);
                        }); // end .getJSON
                     }); // end button
 
-        
+        //see chart of total price
+
+              $("button#see_total_price").click(function() {
+     $("#div1").hide(1000);
+     $("#footer").show(1000);
+              var table2_items = [];
+                       var i = 0;
+                       var airtable_read_endpoint =
+                       "https://api.airtable.com/v0/appENaVzPyNLQlumN/final_shirts?api_key=keytLf7m73sOFf472&maxRecords=40&view=Grid%20view";
+                       var table2_dataSet = [];
+                       $.getJSON(airtable_read_endpoint, function(result) {
+                              $.each(result.records, function(key,value) {
+                                  table2_items = [];
+                                      table2_items.push(value.fields.Outfits_code);
+                                      table2_items.push(value.fields.Total_price);
+                                      table2_dataSet.push(table2_items);
+                                      console.log(table2_items);
+                               }); // end .each
+                               console.log(table2_dataSet);
+                              $('#table2').DataTable( {
+                                  data: table2_dataSet,
+                                  retrieve: true,
+                                  ordering: false,
+                                  columns: [
+                                      { title: "Outfits_code",
+                                        defaultContent:""},
+                                      { title: "Total_price",
+                                        defaultContent:""},
+                                  ] // rmf columns
+                              } ); // end dataTable
+
+
+                  var chart = c3.generate({
+                                       data: {
+                                           columns: table2_dataSet,
+                                           type : 'bar'
+                                       },
+                                       color: {
+                                               pattern: ['#aec7e8','#aec7e8','#1f77b4','#1f77b4','#1f77b4','#1f77b4','#1f77b4','#16547F','#16547F','#16547F']
+                                           },
+                                       axis: {
+                                                   x: {label: 'Outfits code'},
+                                                   y: {label: '# of Total Price (HKD)'}
+                                                 },
+                                       grid: {
+                                                 y: {
+                                                     lines: [
+                                                         {value: 300, text: 'Label 300 for y', position: 'start'},
+                                                        {value: 600, text: 'Label 600 for y', position: 'start'},
+                                                        {value: 900, text: 'Label 900 for y', position: 'start'},
+                                                             ]
+                                                     }
+                                               },
+                                              bar: {
+                                                  title: "Total Price for Each Occasion:",
+
+                                              width: {
+                                                          ratio:0.4 // this makes bar width 50% of length between ticks
+                                                      },
+                                                      // or
+                                                      //width: 100 // this makes bar width 100px
+                                                     },
+                                   });
+
+                            }); // end .getJSON
+                         }); // end button
 
 
 }); // document ready

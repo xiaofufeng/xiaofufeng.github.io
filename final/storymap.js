9cd7ee9e1af51cb554417a945f1285ad630624f5
fengxiaofu <xiaofufeng2003@163.com>
2018-11-16 16:44:38 +0800

    update

--- final/storymap.js
+++ final/storymap.js
@@ -10,7 +10,7 @@ var storymap_data = {
       date: "2018",
       text: {
         headline: "WHERE TO BUY?",
-        text: "<p>You can find many shopping malls in Hong Kong.<br>Click button at right for more information!</p>"
+        text: "<p>You can find many shopping malls in Hong Kong.<br>Kowloon:count 9<br>New Territories:count 9<br>Hong Kong Island:count 2<br>Click button at right for more information!</p>"
       },
       media: {
         url: "https://images.pexels.com/photos/264489/pexels-photo-264489.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350",
@@ -32,6 +32,7 @@ var storymap_data = {
         zoom: 16,
         line: true
       },
+      icon:{icon},
       media: {
         url: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Miramar_Shopping_Centre_Atrium_2015.JPG/500px-Miramar_Shopping_Centre_Atrium_2015.JPG",
         type: 		"image",
@@ -39,26 +40,152 @@ var storymap_data = {
         caption: ""
       }
     },
-     {
-      date: "2018",
-      text: {
-        headline: "Cityplaza",
-        text: "<p>Tel：(852) 2907 0302<br>地址：太古城道18號太古城中心4樓 401店<br>營業時間：星期日至四及公眾假期 上午11:00 - 下午10:00 星期五、六及公眾假期前一天 上午11:00 - 下午11:00</p> "
-      },
-      location: {
-        name: "",
-        icon:icon,
-        lat: 22.285833,
-        lon: 114.217778,
-        zoom: 16,
-        line: true
-      },
-      media: {
-        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Cityplaza_Phase_1_Atrium_201401.jpg/400px-Cityplaza_Phase_1_Atrium_201401.jpg",
-        type: "image",
-        credit: "Hong Kong",
-        caption: ""
-      }
+    {
+     date: "2018",
+     text: {
+       headline: "Olympian City",
+       text: "<p>Tel：(852) 2272 4977<br>地址：九龍奧海城2期UG 40-45店<br>營業時間：星期日至四及公眾假期 上午11:00 - 下午10:00 星期五、六及公眾假期前一天 上午11:00 - 下午11:00</p>"
+     },
+     location: {
+       name: "",
+       icon: icon,
+       lat: 22.3194,
+       lon: 114.1583,
+       zoom: 16,
+       line: true
+     },
+     media: {
+       url: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Olympian_City_Phase_2_Entrance_201205.jpg/560px-Olympian_City_Phase_2_Entrance_201205.jpg",
+       type: 		"image",
+       credit: "Olympian City",
+       caption: ""
+     }
+    },
+    {
+     date: "2018",
+     text: {
+       headline: "apm",
+       text: "<p>Tel：(852) 3148 9111<br>地址：九龍觀塘道418號創紀之城 5 期 apm L3-Xsite 店<br>營業時間：每日上午11:00 - 午夜12:00</p>"
+     },
+     location: {
+       name: "",
+       icon: icon,
+       lat: 22.312536,
+       lon: 114.225056,
+       zoom: 16,
+       line: true
+     },
+     media: {
+       url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Millennium_City_5_2014.jpg/440px-Millennium_City_5_2014.jpg",
+       type: 		"image",
+       credit: "apm",
+       caption: ""
+     }
+    },
+    {
+     date: "2018",
+     text: {
+       headline: "MOKO",
+       text: "<p>Tel：(852) 2777 7802<br>地址：九龍旺角太子道西193號新世紀廣場2樓217-235,238店<br>營業時間：星期日至四及公眾假期 上午11:00 - 下午10:00 星期五、六及公眾假期前一天 上午11:00 - 下午11:00</p>"
+     },
+     location: {
+       name: "",
+       icon: icon,
+       lat: 22.32317,
+       lon: 114.17221,
+       zoom: 16,
+       line: true
+     },
+     media: {
+       url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/NewCenturyPlace.JPG/560px-NewCenturyPlace.JPG",
+       type: 		"image",
+       credit: "MOKO",
+       caption: ""
+     }
+    },
+    {
+     date: "2018",
+     text: {
+       headline: "Harbour City",
+       text: "<p>Tel：(852) 2175 0810<br>地址：九龍尖沙咀海港城港威商場3樓3231-2店<br>營業時間：每日上午10:00 - 下午10:00</p>"
+     },
+     location: {
+       name: "",
+       icon: icon,
+       lat: 22.29784,
+       lon: 114.16797,
+       zoom: 16,
+       line: true
+     },
+     media: {
+       url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Harbour_City_flags.jpg/500px-Harbour_City_flags.jpg",
+       type: 		"image",
+       credit: "Harbour City",
+       caption: ""
+     }
+    },
+    {
+     date: "2018",
+     text: {
+       headline: "Plaza Hollywood",
+       text: "<p>Tel：(852) 2955 4100<br>地址：九龍鑽石山龍蟠街三號荷里活廣場地下G101 & G101A店<br>營業時間：星期日至四及公眾假期 上午11:00 - 下午10:00 星期五、六及公眾假期前一天 上午11:00 - 下午11:00</p>"
+     },
+     location: {
+       name: "",
+       icon: icon,
+       lat: 22.34071,
+       lon: 114.20206,
+       zoom: 16,
+       line: true
+     },
+     media: {
+       url: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/HK_Plaza_Hollywood_Void_View1.jpg/500px-HK_Plaza_Hollywood_Void_View1.jpg",
+       type: 		"image",
+       credit: "Plaza Hollywood",
+       caption: ""
+     }
+    },
+    {
+     date: "2018",
+     text: {
+       headline: "Lok Fu Place",
+       text: "<p>Tel：(852) 2337 3837<br>地址：九龍橫頭磡聯合道198號樂富廣場1151-1156店<br>營業時間：星期日至四及公眾假期 上午11:00 - 下午10:00 星期五、六及公眾假期前一天 上午11:00 - 下午11:00</p>"
+     },
+     location: {
+       name: "",
+       icon: icon,
+       lat: 22.3374,
+       lon: 114.18716,
+       zoom: 16,
+       line: true
+     },
+     media: {
+       url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Lok_Fu_Plaza_Phase1_New_Atrium_201103.jpg/560px-Lok_Fu_Plaza_Phase1_New_Atrium_201103.jpg",
+       type: 		"image",
+       credit: "Lok Fu Place",
+       caption: ""
+     }
+    },
+    {
+     date: "2018",
+     text: {
+       headline: "Whampoa Garden",
+       text: "<p>Tel：(852) 2755 6977<br>地址：九龍紅磡黃埔新天地時尚坊(二期) G42-43及B1-4店<br>營業時間：星期日至四及公眾假期 上午11:00 - 下午10:00 星期五、六及公眾假期前一天 上午11:00 - 下午11:00</p>"
+     },
+     location: {
+       name: "",
+       icon: icon,
+       lat: 22.3039,
+       lon: 114.1922,
+       zoom: 16,
+       line: true
+     },
+     media: {
+       url: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Fashion_World_Interior_2014.jpg/240px-Fashion_World_Interior_2014.jpg",
+       type: 		"image",
+       credit: "Whampoa Garden",
+       caption: ""
+     }
     },
     {
      date: "2018",
@@ -81,27 +208,241 @@ var storymap_data = {
        caption: ""
      }
    },
-   {
-    date: "2018",
-    text: {
-      headline: "East Point City",
-      text: "<p>Tel：(852) 2628 5888<br>地址：新界將軍澳坑口東港城178-179 & 191-192店<br>營業時間：星期日至四及公眾假期 上午11:00 - 下午10:00 星期五、六及公眾假期前一天 上午11:00 - 下午11:00</p>"
-    },
-    location: {
-      name: "",
-      icon: icon,
-      lat: 22.31636,
-      lon: 114.26516,
-      zoom: 16,
-      line: true
-    },
-    media: {
-      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/2014%E5%B9%B4%E6%9D%B1%E6%B8%AF%E5%9F%8E%E4%B8%80%E6%A8%93%E5%B1%95%E8%A6%BD%E5%A0%B4.jpg/500px-2014%E5%B9%B4%E6%9D%B1%E6%B8%AF%E5%9F%8E%E4%B8%80%E6%A8%93%E5%B1%95%E8%A6%BD%E5%A0%B4.jpg",
-      type: 		"image",
-      credit: "Hong Kong",
-      caption: ""
-    }
-  },
+
+{
+ date: "2018",
+ text: {
+   headline: "World Trade Centre",
+   text: "<p>Tel：(852) 2881 5101<br>地址：銅鑼灣告士打道280 號wtc more 世貿中心P5樓<br>營業時間：星期日至四及公眾假期 上午11:00 - 下午10:00 星期五、六及公眾假期前一天 上午11:00 - 下午11:00</p>"
+ },
+ location: {
+   name: "",
+   icon: icon,
+   lat: 22.2817,
+   lon: 114.1841,
+   zoom: 16,
+   line: true
+ },
+ media: {
+   url: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Wtc_more_Enterance_200902.jpg/400px-Wtc_more_Enterance_200902.jpg",
+   type: 		"image",
+   credit: "World Trade Centre",
+   caption: ""
+ }
+},
+{
+ date: "2018",
+ text: {
+   headline: "Cityplaza",
+   text: "<p>Tel：(852) 2907 0302<br>地址：太古城道18號太古城中心4樓 401店<br>營業時間：星期日至四及公眾假期 上午11:00 - 下午10:00 星期五、六及公眾假期前一天 上午11:00 - 下午11:00</p> "
+ },
+ location: {
+   name: "",
+   icon:icon,
+   lat: 22.285833,
+   lon: 114.217778,
+   zoom: 16,
+   line: true
+ },
+ media: {
+   url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Cityplaza_Phase_1_Atrium_201401.jpg/400px-Cityplaza_Phase_1_Atrium_201401.jpg",
+   type: "image",
+   credit: "Cityplaza",
+   caption: ""
+ }
+},
+{
+ date: "2018",
+ text: {
+   headline: "Tmtplaza",
+   text: "<p>Tel：(852) 2618 4311<br>地址：新界屯門市廣場一期2樓2001-2011及2021-2023店<br>營業時間：星期日至四及公眾假期 上午11:00 - 下午10:00 星期五、六及公眾假期前一天 上午11:00 - 下午11:00</p>"
+ },
+ location: {
+   name: "",
+   icon: icon,
+   lat: 22.39264,
+   lon: 113.97665,
+   zoom: 16,
+   line: true
+ },
+ media: {
+   url: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Tmtplaza_Atrium_201403.jpg/500px-Tmtplaza_Atrium_201403.jpg",
+   type: 		"image",
+   credit: "Tmtplaza",
+   caption: ""
+ }
+},
+{
+ date: "2018",
+ text: {
+   headline: "East Point City",
+   text: "<p>Tel：(852) 2628 5888<br>地址：新界將軍澳坑口東港城178-179 & 191-192店<br>營業時間：星期日至四及公眾假期 上午11:00 - 下午10:00 星期五、六及公眾假期前一天 上午11:00 - 下午11:00</p>"
+ },
+ location: {
+   name: "",
+   icon: icon,
+   lat: 22.31636,
+   lon: 114.26516,
+   zoom: 16,
+   line: true
+ },
+ media: {
+   url: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/2014%E5%B9%B4%E6%9D%B1%E6%B8%AF%E5%9F%8E%E4%B8%80%E6%A8%93%E5%B1%95%E8%A6%BD%E5%A0%B4.jpg/500px-2014%E5%B9%B4%E6%9D%B1%E6%B8%AF%E5%9F%8E%E4%B8%80%E6%A8%93%E5%B1%95%E8%A6%BD%E5%A0%B4.jpg",
+   type: 		"image",
+   credit: "Hong Kong",
+   caption: ""
+ }
+},
+{
+date: "2018",
+text: {
+  headline: "New Town Plaza",
+  text: "<p>Tel：(852) 2606 1126<br>地址：新界沙田新城市廣場一期225店<br>營業時間：星期日至四及公眾假期 上午11:00 - 下午10:00 星期五、六及公眾假期前一天 上午11:00 - 下午11:00</p>"
+},
+location: {
+  name: "",
+  icon: icon,
+  lat: 22.381947,
+  lon: 114.188309,
+  zoom: 16,
+  line: true
+},
+media: {
+  url: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/NewTownPlaza_20070831.jpg/560px-NewTownPlaza_20070831.jpg",
+  type: 		"image",
+  credit: "New Town Plaza",
+  caption: ""
+}
+},
+{
+date: "2018",
+text: {
+ headline: "Citywalk",
+ text: "<p>Tel：(852) 2941 0471<br>地址：新界荃灣楊屋道1號荃新天地1樓128, 137-146店<br>營業時間：星期日至四及公眾假期 上午11:00 - 下午10:00 星期五、六及公眾假期前一天 上午11:00 - 下午11:00</p>"
+},
+location: {
+ name: "",
+ icon: icon,
+ lat: 22.37,
+ lon: 114.113889,
+ zoom: 16,
+ line: true
+},
+media: {
+ url: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/HK_Citywalk_Shopping_Mall.jpg/440px-HK_Citywalk_Shopping_Mall.jpg",
+ type: 		"image",
+ credit: "Citywalk",
+ caption: ""
+}
+},
+{
+date: "2018",
+text: {
+headline: "Maritime Square",
+text: "<p>Tel：(852) 2495 6271<br>地址：新界青衣青敬路33號青衣城1樓108C店<br>營業時間：星期日至四及公眾假期 上午11:00 - 下午10:00 星期五、六及公眾假期前一天 上午11:00 - 下午11:00</p>"
+},
+location: {
+name: "",
+icon: icon,
+lat: 22.3557,
+lon: 114.1082,
+zoom: 16,
+line: true
+},
+media: {
+url: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Maritime_Square_Enterance_2017.jpg/560px-Maritime_Square_Enterance_2017.jpg",
+type: 		"image",
+credit: "Maritime Square",
+caption: ""
+}
+},
+
+{
+ date: "2018",
+ text: {
+   headline: "Tai Po Mega Mall",
+   text: "<p>Tel：(852) 2667 1977<br>地址：新界大埔超級城B區2樓212-213店<br>營業時間：星期日至四及公眾假期 上午11:00 - 下午10:00 星期五、六及公眾假期前一天 上午11:00 - 下午11:00</p>"
+ },
+ location: {
+   name: "",
+   icon: icon,
+   lat: 22.451511,
+   lon: 114.169475,
+   zoom: 16,
+   line: true
+ },
+ media: {
+   url: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Tai_Po_Mega_Mall_Zone_C_Void_200905.jpg/440px-Tai_Po_Mega_Mall_Zone_C_Void_200905.jpg",
+   type: 		"image",
+   credit: "Tai Po Mega Mall",
+   caption: ""
+ }
+},
+
+{
+ date: "2018",
+ text: {
+   headline: "Yuen Long Plaza",
+   text: "<p>Tel：(852) 2777 5380<br>地址：新界元朗青山公路元朗廣場二樓201-220a店<br>營業時間：每日上午10:00 - 下午10:00</p>"
+ },
+ location: {
+   name: "",
+   icon: icon,
+   lat: 22.444827,
+   lon: 114.023996,
+   zoom: 16,
+   line: true
+ },
+ media: {
+   url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Yuen_Long_Plaza.JPG/560px-Yuen_Long_Plaza.JPG",
+   type: 		"image",
+   credit: "Yuen Long Plaza",
+   caption: ""
+ }
+},
+
+{
+ date: "2018",
+ text: {
+   headline: "T Town",
+   text: "<p>Tel：(852) 2816 1468<br>地址：新界元朗天水圍天華路30號天頌苑T Town South一樓S122-124店<br>營業時間：每日上午10:00 - 下午10:00</p>"
+ },
+ location: {
+   name: "",
+   icon: icon,
+   lat: 22.461667,
+   lon: 113.997222,
+   zoom: 16,
+   line: true
+ },
+ media: {
+   url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/T_Town.jpg/500px-T_Town.jpg",
+   type: 		"image",
+   credit: "T Town",
+   caption: ""
+ }
+},
+{
+ date: "2018",
+ text: {
+   headline: "MetroPlaza",
+   text: "<p>Tel：(852) 2661 0618<br>地址：新界葵芳興芳路223號新都會廣場L3 層336號舖<br>營業時間：每日上午10:00 - 下午10:00</p>"
+ },
+ location: {
+   name: "",
+   icon: icon,
+   lat: 22.357608,
+   lon: 114.126417,
+   zoom: 16,
+   line: true
+ },
+ media: {
+   url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/T_Town.jpg/500px-T_Town.jpg",
+   type: 		"image",
+   credit: "MetroPlaza",
+   caption: ""
+ }
+},
     ] // end of slides
   }
 }
